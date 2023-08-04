812b7a4aa8eb9516f094e2672cc88ea82bc4a067
Andrew James <mygithubacc3198@gmail.com>
2023-08-04 23:42:46 +0530

    Did some styling in SearchResults component

--- src/components/SearchResults/SearchReults.js
+++ src/components/SearchResults/SearchReults.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { searchSliceAsync } from '../../features/searchSlice';
+import styles from './SearchResults.module.css';
 
 const SearchResults = () => {
 
@@ -11,31 +12,40 @@ const SearchResults = () => {
 
     useEffect(() => {
         dispatch(searchSliceAsync(query));
-    },[dispatch, query]);
+    }, [dispatch, query]);
 
-    if(loading)
+    if (loading)
         return <div>loading...</div>
 
-    if(error)
+    if (error)
         return <div>{error}</div>
 
-    if(data.results)
-        return ( 
-            <section>
-                <Link to="/">Go back to home...</Link>
-                <h1>Search Results related to :{query}</h1>
-                <ul style={{padding: "1rem 2rem"}}>
-                    {
-                        data.results.map(result => {
-                            return <li key={result.id}>
-                                {result.title ? result.title: result.name}
-                                <p>Type: {result.media_type}</p>
-                            </li>
-                        })
-                    }
-                </ul>
-            </section>
-        );
+    return (
+        <section className={styles.searchResults_section}>
+            <Link to="/">Go back to home...</Link>
+            <h1>Search Results related to :{query}</h1>
+            {
+                data.results &&
+                data.results.map(result => {
+                    return <article className={styles.result_item} key={result.id}>
+                        <div className={styles.image_container}>
+                            <img
+                                src={`https://www.themoviedb.org/t/p/w94_and_h141_bestv2/${result.poster_path}`}
+                                alt={result.title ? result.title : result.name}
+                            />
+                        </div>
+
+                        <div className={styles.details}>
+                            <Link to={`/${result.media_type}/${result.id}`}>
+                                <b>{result.title ? result.title : result.name}</b>
+                            </Link>
+                            <p>{result.overview}</p>
+                        </div>
+                    </article>
+                })
+            }
+        </section>
+    );
 }
- 
+
 export default SearchResults;
