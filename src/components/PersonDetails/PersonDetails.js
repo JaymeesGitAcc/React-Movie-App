5fbc54a61b6bbcea480ccf46a2a4481dfde088f1
Andrew James <mygithubacc3198@gmail.com>
2023-08-18 23:34:21 +0530

    Styled the PersonDetails component

--- src/components/PersonDetails/PersonDetails.js
+++ src/components/PersonDetails/PersonDetails.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Header from '../Header/Header';
 import styles from './PersonDetails.module.css';
@@ -26,33 +26,94 @@ const PersonDetails = () => {
         return url;
     }
 
-    if(loading)
+    function resolveGender(num) {
+        switch (num) {
+            case 1: return 'Female';
+            case 2: return 'Male';
+            default: return 'Not Specified';
+        }
+    }
+
+    if (loading)
         return <div>Loading...</div>
-    
-    if(error)
+
+    if (error)
         return <div>{error}</div>
 
-    return ( 
+    return (
         <>
-            <Header/>
-            <p>Person ID: {personID}</p>
+            <Header />
 
             <section className={styles.details_section}>
-                <div className={styles.flex}>
+                <div className={styles.info_container}>
 
                     <div className={styles.image_container}>
                         <img src={imageURL(data.profile_path)} alt="" />
                     </div>
 
                     <article className={styles.about_person}>
-                        <h1>{data.name}</h1>
-                        <p>{data.biography}</p>
+                        <h1 className={styles.person_name}>{data.name}</h1>
+
+                        <div className={styles.biography}>
+                            <h3>Biography</h3>
+                            <p>{data.biography}</p>
+                        </div>
+
+                        <div className={styles.personal_info}>
+                            <h3 className={styles.personal_info_head}>Personal Info</h3>
+
+                            <div className={styles.lists_container}>
+
+                                <ul>
+                                    <li>
+                                        <h4>Known For</h4>
+                                        <p>{data.known_for_department}</p>
+                                    </li>
+                                    <li>
+                                        <h4>Gender</h4>
+                                        <p>{resolveGender(data.gender)}</p>
+                                    </li>
+
+                                    <li>
+                                        <h4>Date of Birth</h4>
+                                        <p>{data.birthday}</p>
+                                    </li>
+
+                                    {
+                                        data.deathday &&
+                                        <li>
+                                            <h4>Date of Death</h4>
+                                            <p>{data.deathday}</p>
+                                        </li>
+                                    }
+                                    <li>
+                                        <h4>Place of Birth</h4>
+                                        <p>{data.place_of_birth}</p>
+                                    </li>
+
+                                </ul>
+
+                                {
+                                    data.also_known_as &&
+                                    <ul>
+                                        <h4>Also Known As</h4>
+                                        {
+                                            data.also_known_as.map((item, index) => {
+                                                return <li key={index}>{item}</li>
+                                            })
+                                        }
+                                    </ul>
+                                }
+                            </div>
+
+
+                        </div>
                     </article>
 
                 </div>
             </section>
         </>
-     );
+    );
 }
- 
+
 export default PersonDetails;
